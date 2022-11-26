e196f47593c2fb104049e41d8e83a023d04a4a69
Rohan-Naran <100029666+Rohan-Naran@users.noreply.github.com>
2022-11-26 10:54:09 +0000

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -6,3 +6,6 @@ player2_score = 0;
 
 document.getElementById("player1_name").innerHTML=player1_name;
 document.getElementById("player2_name").innerHTML=player2_name;
+
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score".innerHTML=player2_score;
